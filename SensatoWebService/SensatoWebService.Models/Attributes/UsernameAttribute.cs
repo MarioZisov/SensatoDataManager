16a257c0740fe9944de01cd7875dd8bfc964dd2c
zisov4eto <zisov4eto@gmail.com>
2016-12-02 14:34:50 +0200

    Add custom attribute and Name property for Hive.

---
+++ SensatoWebService/SensatoWebService.Models/Attributes/UsernameAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace SensatoWebService.Models.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class UsernameAttribute : ValidationAttribute
+    {
+        private const string Pattern = @"^[A-Za-z0-9]+[\w-]+[a-zA-Z]+[\w-]+$";
+
+        public override bool IsValid(object value)
+        {
+            string username = value.ToString();
+
+            if (Regex.IsMatch(username, Pattern))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
